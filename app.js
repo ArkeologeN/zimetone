74d8fd5f073bcbaa89063730502462027ff81726
ArkeologeN <hamzawaqas.programmer@gmail.com>
2014-06-25 23:49:56 +0500

    support for moment added w/ more pretty code

--- app.js
+++ app.js
@@ -23,28 +23,6 @@ app.configure(function(){
 
 app.engine('ejs',engine);
 
-app.locals.formatStamp = function (date) {
-	var months = [
-		"January", "Febrary", "March",
-		"April", "May","June", "July",
-		"August", "September", "October",
-		"November", "December"
-	], days = [
-			"Sunday", "Monday", "Tuesday",
-			"Wednesday", "Thursday", "Friday",
-			"Saturday", "Sunday"
-		]
-		
-	var hours = date.getHours()
-		, minutes = date.getMinutes()
-		,ampm = hours >= 12 ? 'pm' : 'am';
-	  hours = hours % 12;
-	  hours = hours ? hours : 12; // the hour '0' should be '12'
-	  minutes = minutes < 10 ? '0'+minutes : minutes;
-		
-	return months[date.getMonth()]+" "+ date.getDate()+", "+date.getFullYear()+" "+hours + ':' + minutes + ' ' + ampm
-}
-
 
 
 app.configure('development', function(){
--- routes/index.js
+++ routes/index.js
@@ -1,84 +1,54 @@
-
-/*
- * GET home page.
- */
-var time = require('time')
-	, async = require('async')
-	, request = require('request')
-
-exports.index = function(req, res){
-    var team = req.param('team');
-    if ( typeof(team) == 'undefined') {
-        // Load popup screen.
-        res.render('join', { title: 'Express' });
-
-    } else {
-        // Make Rest calls etc.
-        var locs = {
-			"Chicago": {
-					time: (new time.Date()).setTimezone("America/Chicago"),
-			},
-			"Karachi": {
-					time: (new time.Date()).setTimezone("Asia/Karachi"),
-			},
-			"Sydney": {
-					time: (new time.Date()).setTimezone("Australia/Sydney"),
-			},
-			"Paris": {
-					time: (new time.Date()).setTimezone("Europe/Paris"),
-			}
-		}
-		
-		/*
-		 * var funcs = []
-		for (var x in locs) {
-			var url = 'http://api.openweathermap.org/data/2.5/weather?q='+x
-			console.log(url);
-			funcs.push(function(callback) {
-				request.get(url, function(err, res, body) {
-					console.log(body);
-					callback(null, body);
-				});
-			});
-		}
-		*/
-		
-		async.parallel([
-			function(cb) {
-				request.get('http://api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial', function(err, res, body) {
-					cb(null, body);
-				});
-			},
-			function(cb) {
-				request.get('http://api.openweathermap.org/data/2.5/weather?q=Karachi&units=imperial', function(err, res, body) {
-					cb(null, body);
-				});
-			},
-			function(cb) {
-				request.get('http://api.openweathermap.org/data/2.5/weather?q=Sydney&units=imperial', function(err, res, body) {
-					cb(null, body);
-				});
-			},
-			function(cb) {
-				request.get('http://api.openweathermap.org/data/2.5/weather?q=Paris&units=imperial', function(err, res, body) {
-					cb(null, body);
-				});
-			}
-		], function(err, results) {
-			if ( err )
-				console.log(err)
-				
-			var counter = 0;
-			for (var x in locs) {
-				locs[x].geo = JSON.parse(results[counter]);
-				counter++;
-			}
+(function() {
+	"use strict";
+	
+	var async = require('async'),
+		request = require('request'),
+		moment = require('moment-timezone');
+	
+	module.exports.index = function(req, res){
+	    var team = req.param('team');
+	    if ( typeof(team) == 'undefined') {
+	        // Load popup screen.
+	        res.render('join', { title: 'Express' });
+	
+	    } else {
+	        // Make Rest calls etc.
+	        var locations = {
+	        	"Chicago": "America/Chicago",
+	        	"Karachi": "Asia/Karachi",
+	        	"Sydney": "Australia/Sydney",
+	        	"Paris": "Europe/Paris"
+	        }, _locations = {}, funcs = [];
+	        
+	        for (var l in locations) {
+	        	var time = moment().tz(locations[l]).format('MMMM Do YYYY, h:mm:ss a'),
+	        		func = function(fn) {
+	        			request.get('http://api.openweathermap.org/data/2.5/weather?q='+ l +'&units=imperial', function(err, res, body) {
+							if ( !err) {
+								_locations[l].geo = JSON.parse(body);
+							}
+							return fn(err, body);
+						});
+	        		};
+	        		
+	        	_locations[l] = {
+	        		time: time
+	        	};
+	        	
+	        	funcs.push(func);
+	        	
+	        }
 			
-			res.render('index', { 
-				title: team,
-				locs: locs
+			async.parallel(funcs, function(err, results) {
+				if ( err )
+					console.log(err);
+					
+				res.render('index', { 
+					title: team,
+					locs: _locations
+				});
 			});
-		});
-		
-    }
-};
+			
+	    }
+	};
+})();
